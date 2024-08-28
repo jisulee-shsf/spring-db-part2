5a1609097e7b247d41193f337a46da606daf3926
Jisu Lee <jisulee.shsf@gmail.com>
2024-08-29 04:56:59 +0900

    스프링 트랜잭션 전파2 - 복구 REQUIRED

--- springtx/src/main/java/hello/springtx/propagation/MemberService.java
+++ springtx/src/main/java/hello/springtx/propagation/MemberService.java
@@ -3,7 +3,10 @@ package hello.springtx.propagation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.lang.module.ModuleDescriptor;
 
 @Slf4j
 @Service
@@ -27,6 +30,7 @@ public class MemberService {
         log.info("== logRepository 호출 종료==");
     }
 
+    @Transactional
     public void joinV2(String username) {
         Member member = new Member(username);
         Log logMessage = new Log(username);
--- springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -4,6 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.UnexpectedRollbackException;
+
+import java.rmi.UnexpectedException;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -102,6 +105,25 @@ class MemberServiceTest {
         //when
         assertThatThrownBy(() -> memberService.joinV1(username))
                 .isInstanceOf(RuntimeException.class);
+
+        //then: 모든 데이터 롤백
+        assertTrue(memberRepository.find(username).isEmpty());
+        assertTrue(logRepository.find(username).isEmpty());
+    }
+
+    /**
+     * memberService     @Transactional:ON
+     * memberRepository  @Transactional:ON
+     * logRepository     @Transactional:ON -> RuntimeException
+     */
+    @Test
+    void recoverException_fail() {
+        //given
+        String username = "log 예외_recoverException_fail";
+
+        //when
+        assertThatThrownBy(() -> memberService.joinV2(username))
+                .isInstanceOf(UnexpectedRollbackException.class);
 
         //then: 모든 데이터 롤백
         assertTrue(memberRepository.find(username).isEmpty());
